b67f22d4d77732c70ccb036d703b33f4ce5565d9
zaehorang <zaehorang2@naver.com>
2023-09-07 19:32:21 +0900

    add: Home 화면에 NavigationBar, BarItem추가

--- DD-iOS/DD-iOS/Controller/HomeViewController.swift
+++ DD-iOS/DD-iOS/Controller/HomeViewController.swift
@@ -12,7 +12,7 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.navigationItem.title = "멍뭉일기"
     }
     
 
