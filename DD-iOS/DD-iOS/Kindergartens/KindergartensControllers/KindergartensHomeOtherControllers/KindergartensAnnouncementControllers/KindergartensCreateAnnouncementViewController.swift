758635bf5ba5a76fb9495903c88da3fb6938a940
zaehorang <zaehorang2@naver.com>
2023-09-14 17:01:48 +0900

    feat: 유치원 공지사항 추가 페이지 네비게이션 바

--- DD-iOS/DD-iOS/Kindergartens/KindergartensControllers/KindergartensHomeOtherControllers/KindergartensAnnouncementControllers/KindergartensCreateAnnouncementViewController.swift
+++ DD-iOS/DD-iOS/Kindergartens/KindergartensControllers/KindergartensHomeOtherControllers/KindergartensAnnouncementControllers/KindergartensCreateAnnouncementViewController.swift
@@ -8,22 +8,31 @@
 import UIKit
 
 class KindergartensCreateAnnouncementViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var kindergartensCreateNaviBar: UINavigationBar!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        addNaviItem()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // 네비게이션 바의 타이틀과 버튼을 추가해주는 메서드
+    func addNaviItem() {
+        
+        let naviItem = UINavigationItem(title: "공지사항 쓰기")
+        naviItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "circle.fill"), style: .plain, target: self, action: #selector(self.profileTapButton))
+        
+        kindergartensCreateNaviBar.items = [naviItem]
     }
-    */
-
+    
+    @objc func profileTapButton() {
+        print(#function)
+    }
+    
+    
+    
 }
