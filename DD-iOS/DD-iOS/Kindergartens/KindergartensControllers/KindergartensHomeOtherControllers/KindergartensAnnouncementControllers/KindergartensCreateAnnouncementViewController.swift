947393eb2a18bbdbc4a91fc62e6a5c211e91ab91
zaehorang <zaehorang2@naver.com>
2023-09-15 19:52:38 +0900

    feat: 홈 화면에 있는 버튼 이미지 변경
    - UIImage위에 Button 올려서 구현

--- DD-iOS/DD-iOS/Kindergartens/KindergartensControllers/KindergartensHomeOtherControllers/KindergartensAnnouncementControllers/KindergartensCreateAnnouncementViewController.swift
+++ DD-iOS/DD-iOS/Kindergartens/KindergartensControllers/KindergartensHomeOtherControllers/KindergartensAnnouncementControllers/KindergartensCreateAnnouncementViewController.swift
@@ -22,12 +22,13 @@ class KindergartensCreateAnnouncementViewController: UIViewController {
     
     // 네비게이션 바의 타이틀과 버튼을 추가해주는 메서드
     func addNaviItem() {
-        
         let naviItem = UINavigationItem(title: "공지사항 쓰기")
         naviItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "circle.fill"), style: .plain, target: self, action: #selector(self.profileTapButton))
         
         kindergartensCreateNaviBar.items = [naviItem]
     }
+    
+    
     
     @objc func profileTapButton() {
         print(#function)
